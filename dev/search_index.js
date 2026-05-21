a88720764e23157015037762c55e59d446d3fe45
Documenter.jl <documenter@juliadocs.github.io>
2026-05-21 21:00:14 +0000

    build based on ecd0ace

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"category":"section","location":"#FEM","page":"Home","text":"Documentation for FEM.\n\n","title":"FEM"}]
+[{"category":"section","location":"api/","page":"API","text":"","title":"API"},{"category":"section","location":"","page":"Home","text":"This package provides basic functionalities for FEM codes.","title":"Home"}]
 }
